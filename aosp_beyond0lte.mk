cb515aa56b59d8bfc2ebc3be3594973dd8444dcc
MODEDGES <ofwargane30@gmail.com>
2025-05-03 16:47:48 +0100

    beyond0lte: The Pixel Project bring-up

--- aosp_beyond0lte.mk
+++ aosp_beyond0lte.mk
@@ -30,11 +30,20 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+## Inherit some common The Pixel Project stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# PixelProject Features
+ TARGET_SUPPORTS_QUICK_TAP := true
+ TARGET_SUPPORTS_GOOGLE_RECORDER := true
+ TARGET_INCLUDE_STOCK_ARCORE := true
+ TARGET_INCLUDE_LIVE_WALLPAPERS := true
+ TARGET_ENABLE_BLUR := true
+ CUSTOM_MAINTAINER := MODEDGES
+ CUSTOM_BUILD_TYPE := OFFICIAL
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_beyond0lte
+PRODUCT_NAME := aosp_beyond0lte
 PRODUCT_DEVICE := beyond0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G970F
